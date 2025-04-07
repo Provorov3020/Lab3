bb179e4a2e97f1e66ff2c5febe4704d27c0b308f
Provorov3020 <120006436+Provorov3020@users.noreply.github.com>
2025-04-07 15:07:33 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,16 @@
+using DotNetCrudWebApi.Movies;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCrudWebApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<MovieModel> Movies { get; set; }
+        public DbSet<DirectorModel> Directors { get; set; }
+        public DbSet<ActorModel> Actors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=./data/MoviesApp.db");
+        }
+    }
+}
