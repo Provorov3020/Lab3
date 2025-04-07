5dc6b42504646f8295caa0510457178372c94eea
Provorov3020 <120006436+Provorov3020@users.noreply.github.com>
2025-04-07 15:06:29 +0300

    Add files via upload

---
+++ DirectorModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetCrudWebApi.Movies
+{
+    public class DirectorModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+    }
+}
+
